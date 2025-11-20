dd7038d5ea6e2cc46edad844e38ef2808b7e017b
324094432 <guy.kaizer@gmail.com>
2025-11-20 17:19:12 +0200

    Update main2.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 # Add the import statements for functions from string_utils.py and equation_utils.py here
+import importlib, string_utils
+importlib.reload(string_utils)
+
+import importlib, equation_utils
+importlib.reload(equation_utils)
 
 from equation_utils import my_solve, build_equations, generate_equation_for_element
 from string_utils import parse_chemical_reaction, count_atoms_in_reaction
