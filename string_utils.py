2c900f475737d684864d8576cfc547d97e072ba4
324094432 <guy.kaizer@gmail.com>
2025-11-22 12:43:58 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,51 +1,33 @@
-
-
-
-def split_before_each_uppercases(formula):
-    if not formula:
-        return []
+def split_before_uppercases(formula):
     start = 0
     end = 1
-    split_formula = []
-    while end<len(formula):
-      if formula[end].isupper():
-        split_formula.append(formula[start:end])
-        start=end
-      end +=1
-    split_formula.append(formula[start:end])
-
-    return split_formula
-
-
-def split_at_first_digit(formula):
-    digit_location=1
-    for ch in formula[1:]:
-      if ch.isdigit():
-        break
-      digit_location += 1
-    if digit_location == len(formula):
-        return formula, 1
-    prefix = formula[:digit_location]
-    number = int(formula[digit_location:])
+    elements_lst = []
     
-    return prefix, number
+    if not formula:
+        return elements_lst
 
+    while end < len(formula):
+        if formula[end].isupper():
+            elements_lst.append(formula[start:end])
+            start = end
+        end+=1  
+     
+    elements_lst.append(formula[start:])
+    
+    return elements_lst
 
+def split_at_digit(formula):
+    for char_index, char in enumerate(formula):
+        if char.isdigit():
+            return formula[:char_index], int(formula[char_index:])
+    return formula, 1
 
 def count_atoms_in_molecule(molecular_formula):
-    """Takes a molecular formula (string) and returns a dictionary of atom counts.
-    Example: 'H2O' → {'H': 2, 'O': 1}"""
-
-    atom_dict={}
-
+    atoms_count_dict = {}
     for atom in split_before_uppercases(molecular_formula):
         atom_name, atom_count = split_at_digit(atom)
-
-        atom_dict[atom_name]=atom_count
-
-    return atom_dict
-
-
+        atoms_count_dict[atom_name] = atoms_count_dict.get(atom_name, 0) + atom_count    
+    return atoms_count_dict
 
 def parse_chemical_reaction(reaction_equation):
     """Takes a reaction equation (string) and returns reactants and products as lists.  
